01bb9b9332fbdb3c096f8b93a4427a63531260f1
kyrgyz00 <bekbolottashpolotov@gmail.com>
2022-10-21 09:50:54 +0600

    first commit

---
+++ lesson7.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+void main() {
+  // int i = 1;
+  // while (i <= 5) {// usloviye()
+  //   print(i);
+  //   ++i;
+  // }
+  // int i = 1;
+  // while (true) {
+  //   print("welcome to dart");
+
+  // }
+
+  // bool isZero = false;
+  // int zero;
+
+  // while (isZero == false) {
+  //   zero = int.parse(stdin.readLineSync()!);
+  //   print("vashe chislo$zero");
+  //   if (zero == 0) {
+  //     isZero = true;
+  //     print("vy vveli 0");
+  //   }
+  // }
+
+  // int i = 10;
+  // print("Dart do-while loop example");
+  // do {
+  //   print(i);
+  //   i++;
+  // } while (i <= 20);
+  // print("The loop is terminated");
+  // while (i <= 20) {
+  //   print(i);
+  // }
+}
---
+++ practise7.dart
@@ -0,0 +1,49 @@
+void main() {
+  //task1
+  // int i = 1;
+  // while (i <= 100) {
+  //   print(i);
+  //   ++i;
+  // }
+
+  // for (int i1 = 1; i1 <= 100; i1++) {
+  //   print(i1);
+  // }
+
+//task2
+  // int i = 11;
+  // while (i < 33) {
+  //   print(i);
+  //   i++;
+  // }
+
+  // for (int i1 = 11; i1 < 33;i1++) {
+  //   print(i1);
+  // }
+
+  // task3
+  // int i = 0;
+  // while (i <= 100) {
+  //   if (i % 2 == 0) {//isEven =  i%2==0
+  //     print(i);
+  //   }
+  //   i++;
+  // }
+  // for (int i = 0; i <= 100; i++) {
+  //   // if (i % 2 == 0) {
+  //     if(i.isEven){
+  //     print(i);
+  //   }
+  //   i++;
+  // }
+
+//task4
+  int i = 0;
+  int m = 0;
+  while (i <= 100) {
+    m = m + i;
+    i++;
+  }
+   print(m);
+
+}
